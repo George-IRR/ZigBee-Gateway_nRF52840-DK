770fa1acb1ad206f1666b53399ddb64fb17407e2
George-IRR <196656380+George-IRR@users.noreply.github.com>
2026-04-20 12:42:31 +0300

    feat: add i2c id read from sensor register

--- zigbee_end_device/bmp180/src/main.c
+++ zigbee_end_device/bmp180/src/main.c
@@ -6,24 +6,26 @@
 int main(void)
 {
     const struct device *i2c_dev = DEVICE_DT_GET(DT_NODELABEL(i2c0));
+    uint8_t address = 0x77;
 
     if (!device_is_ready(i2c_dev)) {
         printk("Cannot init I2C\n");
         return 0;
     }
 
-    printk("I2C scanning \n");
-
-    // Scan I2C address from 0x08 to 0x77 
-    for (uint8_t address = 8; address<= 119; address++) {
-        uint8_t data = 0;
-        
-        // Send empty packet to address / ping
-        if (i2c_write(i2c_dev, &data, 0, address) == 0) {
-            printk("Device found at : 0x%02X\n", address);
-        }
+    // Send empty packet to address / ping
+    uint8_t data = 0;
+    if (i2c_write(i2c_dev, &data, 0, address) == 0) {
+        printk("Device found at : 0x%02X\n", address);
     }
-    printk("Scan finished\n");
+
+    data = 0xD0;
+    uint8_t read_data;
+    i2c_write_read(i2c_dev, address, &data, sizeof(data), &read_data, sizeof(read_data));
+
+    //k_msleep(1000);
+
+    printk("Response: 0x%02X\n", read_data);
     
     return 0;
 }

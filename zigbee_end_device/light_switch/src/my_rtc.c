1575030116239c7b22087e5f33629e859ac67160
George-IRR <196656380+George-IRR@users.noreply.github.com>
2026-06-21 00:43:49 +0300

    feat(rtc): add custom rtc driver

---
+++ zigbee_end_device/light_switch/src/my_rtc.c
@@ -0,0 +1,68 @@
+#include <my_rtc.h>
+
+static My_RTC_Type* get_rtc_pointer(my_rtc_instance_t rtc_idx)
+{
+    switch (rtc_idx) {
+        case MY_RTC_0: return MY_RTC0;
+        case MY_RTC_1: return MY_RTC1;
+        case MY_RTC_2: return MY_RTC2;
+        default:       return ((void*)0); 
+    }
+}
+
+void my_rtc_init(my_rtc_instance_t rtc_idx, uint8_t prescaler)
+{
+    My_RTC_Type *rtc = get_rtc_pointer(rtc_idx);
+    if (!rtc) return; 
+
+    rtc->PRESCALER = prescaler;
+}
+
+void my_rtc_start(my_rtc_instance_t rtc_idx)
+{
+    My_RTC_Type *rtc = get_rtc_pointer(rtc_idx);
+    if (!rtc) return;
+
+    rtc->TASKS_START = 1; 
+}
+
+void my_rtc_stop(my_rtc_instance_t rtc_idx)
+{
+    My_RTC_Type *rtc = get_rtc_pointer(rtc_idx);
+    if (!rtc) return;
+
+    rtc->TASKS_STOP = 1;
+}
+
+uint32_t my_rtc_get_value(my_rtc_instance_t rtc_idx)
+{
+    My_RTC_Type *rtc = get_rtc_pointer(rtc_idx);
+    return rtc ? rtc->COUNTER : 0;
+}
+
+void my_rtc_start_compare(my_rtc_instance_t rtc_idx, uint8_t channel, uint32_t compare_value)
+{
+    My_RTC_Type *rtc = get_rtc_pointer(rtc_idx);
+    
+    // If value is bigger than 24 bits, return
+    if (!rtc || channel > 3 || compare_value > 0x00FFFFFF) return;
+
+    rtc->CC[channel] = compare_value;
+}
+
+bool my_rtc_get_compare_event(my_rtc_instance_t rtc_idx, uint8_t channel)
+{
+    My_RTC_Type *rtc = get_rtc_pointer(rtc_idx);
+    
+    if (!rtc || channel > 3) return false;
+
+    return (rtc->EVENTS_COMPARE[channel] == 1);
+}
+
+void my_rtc_clear_compare_event(my_rtc_instance_t rtc_idx, uint8_t channel)
+{
+    My_RTC_Type *rtc = get_rtc_pointer(rtc_idx);
+    if (!rtc || channel > 3) return;
+
+   rtc->EVENTS_COMPARE[channel] = 0;
+}

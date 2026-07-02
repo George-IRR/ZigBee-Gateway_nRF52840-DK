e4f88d78eb1fb8b927af09d08b336be12f0f5a65
George-IRR <196656380+George-IRR@users.noreply.github.com>
2026-07-02 10:59:37 +0300

    feat: add asserts for peripherals implementation

--- zigbee_end_device/light_switch/src/my_nrf52_timer.c
+++ zigbee_end_device/light_switch/src/my_nrf52_timer.c
@@ -1,4 +1,6 @@
 #include "my_nrf52_timer.h"
+#include <zephyr/sys/__assert.h>
+#include <stddef.h>
 
 static My_TIMER_Type* get_timer_pointer(my_timer_instance_t timer_idx)
 {
@@ -8,15 +10,17 @@ static My_TIMER_Type* get_timer_pointer(my_timer_instance_t timer_idx)
         case MY_TIMER_2: return MY_TIMER2;
         case MY_TIMER_3: return MY_TIMER3;
         case MY_TIMER_4: return MY_TIMER4;
-        default:         return ((void*)0); // return NULL for wrong index
+        default:         return ((void*)0); 
     }
 }
 
 void my_timer_init(my_timer_instance_t timer_idx, uint8_t prescaler)
 {
     My_TIMER_Type *timer = get_timer_pointer(timer_idx);
+    __ASSERT(timer != NULL, "Invalid Timer instance!");
+    
     if (timer != ((void*)0)) {
-        timer->BITMODE   = 3;          // 32 bit
+        timer->BITMODE   = 3;          // 32 bit configuration
         timer->PRESCALER = prescaler;  
     }
 }
@@ -24,6 +28,8 @@ void my_timer_init(my_timer_instance_t timer_idx, uint8_t prescaler)
 void my_timer_start(my_timer_instance_t timer_idx)
 {
     My_TIMER_Type *timer = get_timer_pointer(timer_idx);
+    __ASSERT_NO_MSG(timer != NULL);
+    
     if (timer != ((void*)0)) {
         timer->TASKS_START = 1;
     }
@@ -32,6 +38,8 @@ void my_timer_start(my_timer_instance_t timer_idx)
 void my_timer_stop(my_timer_instance_t timer_idx)
 {
     My_TIMER_Type *timer = get_timer_pointer(timer_idx);
+    __ASSERT_NO_MSG(timer != NULL);
+    
     if (timer != ((void*)0)) {
         timer->TASKS_STOP = 1;
     }
@@ -40,6 +48,9 @@ void my_timer_stop(my_timer_instance_t timer_idx)
 uint32_t my_timer_get_value(my_timer_instance_t timer_idx, uint8_t channel)
 {
     My_TIMER_Type *timer = get_timer_pointer(timer_idx);
+    __ASSERT(timer != NULL, "Invalid Timer instance!");
+    __ASSERT(channel <= 5, "Invalid Timer channel! Must be between 0 and 5.");
+    
     if (timer == ((void*)0)) {
         return 0;
     }
